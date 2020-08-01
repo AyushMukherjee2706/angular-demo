6cceb365ba36b403cae0d40bf959ee62be87fcc1
Ashok_Nitin <ashok_nitin@dell.com>
2020-08-01 17:23:42 +0530

    Login fixed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,8 +6,8 @@ import { AppComponent } from './app.component';
 import { AuthGuardService } from './services/auth-guard.service'
 import { LoginComponent } from './components/login/login.component'
 
-const routes: Routes = [
-  {path: 'dashboard', component: AppComponent, canActivate: [AuthGuardService]},
+const routes: Routes = [  
+  {path: '', component: AppComponent, canActivate: [AuthGuardService]},
   {path: 'upload-file', component: FilesComponent, canActivate: [AuthGuardService]},
   {path:'all-files', component: ListFilesComponent, canActivate: [AuthGuardService]},
   {path:'login', component: LoginComponent}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,14 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { FilesComponent } from './components/files/files.component';
-import { ListFilesComponent } from './components/list-files/list-files.component';
-import { LoginComponent } from './components/login/login.component';
-import { AdminComponent } from './components/admin/admin.component';
-import { FormsModule } from '@angular/forms';
-import { RouterModule } from '@angular/router';
-import { HomeComponent } from './components/home/home.component';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { HttpClientModule } from "@angular/common/http";
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { FilesComponent } from "./components/files/files.component";
+import { ListFilesComponent } from "./components/list-files/list-files.component";
+import { LoginComponent } from "./components/login/login.component";
+import { AdminComponent } from "./components/admin/admin.component";
+import { FormsModule } from "@angular/forms";
+
 
 @NgModule({
   declarations: [
@@ -17,33 +16,10 @@ import { HomeComponent } from './components/home/home.component';
     FilesComponent,
     ListFilesComponent,
     LoginComponent,
-    AdminComponent
+    AdminComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    HttpClientModule,
-    FormsModule
-    // RouterModule.forRoot([  // write a array of routes 
-    //   { 
-    //     path: 'login',
-    //     component : LoginComponent
-  
-    //   },
-    //   { 
-    //     path: 'admin',
-    //     component : AdminComponent
-  
-    //   },
-    //   { 
-    //     path: 'home',
-    //     component : HomeComponent
-  
-    //   }
-    // ])
-    
-  ],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
